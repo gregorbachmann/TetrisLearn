9599e3c5de28dc868021c22d68d344becc15d8fd
gregorbachmann <38691167+gregorbachmann@users.noreply.github.com>
2018-05-07 17:55:21 +0200

    Careful: tetris.py is set to only produce squares, change __init__

---
+++ Q_Training.py
@@ -0,0 +1,226 @@
+import tensorflow as tf
+from collections import deque
+import numpy as np
+import random
+# Define all parameters needed for DeepQNet
+wait = [1, 0, 0, 0]
+left = [0, 1, 0, 0]
+right = [0, 0, 1, 0]
+rotate = [0, 0, 0, 1]
+possible_actions = [wait,left, right, rotate]
+
+# Model parameters
+state_size = [10, 20, 1]  # Our state is a 10x20 matrix with only one channel
+action_size = 4  # We have always 4 actions to choose from
+learning_rate = 0.0002  # Choose some learning rate
+training = True
+
+# Training parameters
+total_episodes = 700  # Run 1000 Tetris games to train
+max_steps = 2000
+batch_size = 64
+
+# Exploration parameters
+explore_start = 1.0  # Exploring parameters at start
+explore_stop = 0.1  # At least this probability
+decay_rate = 0.0001  # Exponential decay
+
+# Q-Learning parameters:
+gamma = 0.99
+
+# Memory parameters
+pretrain_length = batch_size  # Number of elements needed at start in memory queue
+memory_size = 5000
+
+
+# Now populate the memory by random actions
+
+game = tetris()  # Initialize a Tetris game
+game.reset()
+memory = Memory(max_size=memory_size)  # Initialize the Memory queue
+# Reset the graph
+tf.reset_default_graph()
+
+# Instantiate the DQNetwork
+
+deepqnet1 = DeepQNet(state_size=state_size,action_size=action_size,learning_rate=learning_rate)
+# Fill the queue up:
+for i in range(pretrain_length):
+    # First create initial state
+    if i == 0:
+        game.project()
+        state = game.field.reshape(state_size)
+
+    action = random.choice(possible_actions)  # Sample random action
+
+    current_points = game.points
+    if action == 0:
+        game.wait()
+    if action == 1:
+        game.translate_left()
+    if action == 2:
+        game.translate_right()
+    if action == 3:
+        game.rotate()
+    if i % 2 == 0:
+        game.gravitate()
+
+
+    # Determine wheter episode has finished
+    if game.block == None:
+        done = True
+        reward = game.points-current_points  # When n
+        game = tetris()
+        game.reset()  # Initialize new game
+        game.project()
+        next_state = game.field.reshape(state_size)
+        memory.add((state, action, reward, next_state, done))  # Add experience to memory
+        state = next_state
+
+    else:
+        done = False
+        game.project()
+        next_state = game.field.reshape(state_size)
+        reward = game.points - current_points
+        memory.add((state, action, reward, next_state, done))  # Add experience to memory
+        state = next_state
+
+
+# Start training the model
+
+saver = tf.train.Saver()
+
+if training == True:
+    rewards_list = []
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        game = tetris()  # Initialize a Tetris game
+
+        decay_step = 0
+
+        for episode in range(total_episodes):
+            print(episode)
+            game.reset()
+            step = 0
+
+            game.project()
+            state = game.field.reshape(state_size)
+
+            while step < max_steps:
+                step += 1
+                decay_step += 1
+                current = game.points
+
+                exp_exp_tradeoff = np.random.rand()
+                explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
+
+                if explore_probability > exp_exp_tradeoff:
+                    action = random.choice(possible_actions)
+
+                else:
+                    Qs = sess.run(deepqnet1.output, feed_dict={deepqnet1.inputs_: state.reshape((1, *state.shape))})
+                    action = np.argmax(Qs)
+                    action = possible_actions[int(action)]
+                if action[0] == 1:
+                    game.wait()
+                if action[1] == 1:
+                    game.translate_left()
+                if action[2] == 1:
+                    game.translate_right()
+                if action[3] == 1:
+                    game.rotate()
+                if step % 2 == 0:
+                    game.gravitate()
+
+
+
+                if game.block == None:
+                    done = True
+                    total_reward = game.points
+                    game = tetris()
+                    game.reset()
+                    game.project()
+                    next_state = game.field.reshape(state_size)
+                    step = max_steps
+                    print('Episode: {}'.format(episode),
+                          'Total reward: {}'.format(total_reward),
+                          'Training loss: {:.4f}'.format(loss),
+                          'Explore P: {:.4f}'.format(explore_probability))
+                    rewards_list.append((episode, total_reward))
+                    memory.add((state, action, reward, next_state, done))
+
+                else:
+                    done =False
+                    game.project()
+                    reward = game.points - current
+                    next_state = game.field.reshape(state_size)
+                    memory.add((state, action, reward, next_state, done))
+                    state = next_state
+
+                # Learning part
+
+                batch = memory.sample(batch_size)
+                states = np.array([each[0] for each in batch], ndmin=3)
+                actions = np.array([each[1] for each in batch])
+                rewards = np.array([each[2] for each in batch])
+                next_states = np.array([each[3] for each in batch])
+                dones = np.array([each[4] for each in batch])
+
+                target_Qs_batch = []
+                target_Qs = sess.run(deepqnet1.output, feed_dict={deepqnet1.inputs_: next_states})
+
+                for i in range(len(batch)):
+                    terminal = dones[i]
+                    if terminal:
+                        target_Qs_batch.append(rewards[i])
+                    else:
+                        target = rewards[i] + gamma * np.max(target_Qs[i])
+                        target_Qs_batch.append(target)
+                targets = np.array([each for each in target_Qs_batch])
+                loss, _ = sess.run([deepqnet1.loss, deepqnet1.optimizer], feed_dict={deepqnet1.inputs_: states,
+                                deepqnet1.target_Q: targets, deepqnet1.actions_: actions})
+
+
+# Now after training, let the NNet play the game
+
+game_new = tetris()
+
+game_new.reset()
+counter=0
+moves=2
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    while game_new.block!=None:
+        for i in range(moves):
+            game_new.project()
+            state = game_new.field.reshape((1,10,20,1))
+            output = sess.run(deepqnet1.output, feed_dict={deepqnet1.inputs_: state})
+            action = np.argmax(output)
+            if action==0:
+                game_new.translate_left()
+            if action==1:
+                game_new.translate_right()
+            if action==2:
+                game_new.rotate()
+            if action==3:
+                game_new.wait()
+            if game.block==None:
+                break
+            game_new.project()
+            plt.figure(1); plt.clf()
+            plt.imshow(game_new.field)
+            plt.title('Number ' + str(i+counter))
+            plt.pause(0.02)
+
+        counter+=1
+
+        game_new.gravitate()
+        if game_new.block == None:
+            break
+        game_new.project()
+        plt.figure(1); plt.clf()
+        plt.imshow(game_new.field)
+        plt.title('Number ' + str(i+counter))
+        plt.pause(0.02)
--- tetris.py
+++ tetris.py
@@ -16,95 +16,134 @@ class blocks:
         self.pos = pos
         self.shape = shape
         self.orientation = orientation
-
+        self.check_right = 3
+        self.check_left = -1
     def reset(self):
         self.pos = [0, 5]
         self.orientation = 0
-        self.shape = 1
+        self.shape = 5
 
     def get_matrix(self):
         if self.shape == 0:  # STICK
             if self.orientation == 0 or self.orientation == 2:
                 A = np.ones((1, 4))
+                self.check_right = A.shape[1]
+                self.check_left = -1
             if self.orientation == 1 or self.orientation == 3:
                 A = np.ones((4, 1))
+                self.check_right = A.shape[1]
+                self.check_left = -1
 
         if self.shape == 1:  # TRIANGLE
             if self.orientation == 0:
                 A = np.zeros((2, 3))
                 A[1, 0:3] = 1
                 A[0, 1] = 1
+                self.check_right = A.shape[1]
+                self.check_left = -1
             if self.orientation == 1:
                 A = np.zeros((3, 2))
                 A[0:3, 0] = 1
                 A[1, 1] = 1
+                self.check_right = 1
+                self.check_left = -1
             if self.orientation == 2:
                 A = np.zeros((2, 3))
                 A[0, 0:3] = 1
                 A[1, 1] = 1
+                self.check_right = 2
+                self.check_left = 0
             if self.orientation == 3:
                 A = np.zeros((3, 2))
                 A[0:3, 1] = 1
                 A[1, 0] = 1
+                self.check_right = A.shape[1]
+                self.check_left = 0
 
         if self.shape == 2:  # LEFT L
             if self.orientation == 0:
                 A = np.zeros((2, 3))
                 A[1, 0:3] = 1
                 A[0, 0] = 1
+                self.check_right = A.shape[1]
+                self.check_left = -1
             if self.orientation == 1:
                 A = np.zeros((3, 2))
                 A[0:3, 0] = 1
                 A[0, 1] = 1
+                self.check_right = 1
+                self.check_left = -1
             if self.orientation == 2:
                 A = np.zeros((2, 3))
                 A[0, 0:3] = 1
                 A[1, 2] = 1
+                self.check_right = A.shape[1]
+                self.check_left = 1
             if self.orientation == 3:
                 A = np.zeros((3, 2))
                 A[0:3, 1] = 1
                 A[2, 0] = 1
+                self.check_right = A.shape[1]
+                self.check_left = -1
 
         if self.shape == 3:  # RIGHT L
             if self.orientation == 0:
                 A = np.zeros((2, 3))
                 A[1, 0:3] = 1
                 A[0, 2] = 1
+                self.check_right = A.shape[1]
+                self.check_left = -1
             if self.orientation == 1:
                 A = np.zeros((3, 2))
                 A[0:3, 0] = 1
                 A[2, 1] = 1
+                self.check_right = A.shape[1]
+                self.check_left = -1
             if self.orientation == 2:
                 A = np.zeros((2, 3))
                 A[0, 0:3] = 1
                 A[1, 0] = 1
+                self.check_right = 1
+                self.check_left = -1
             if self.orientation == 3:
                 A = np.zeros((3, 2))
                 A[0:3, 1] = 1
                 A[0, 0] = 1
+                self.check_right = A.shape[1]
+                self.check_left = 0
 
         if self.shape == 4:  # LEFT S
             if self.orientation == 0 or self.orientation == 2:
                 A = np.zeros((2, 3))
                 A[0, 1:3] = 1
                 A[1, 0:2] = 1
+                self.check_right = 2
+                self.check_left = -1
             if self.orientation == 1 or self.orientation == 3:
                 A = np.zeros((3, 2))
                 A[0:2, 0] = 1
                 A[1:3, 1] = 1
+                self.check_right = A.shape[1]
+                self.check_left = 0
 
         if self.shape == 4:  # RIGHT S
             if self.orientation == 0 or self.orientation == 2:
                 A = np.zeros((2, 3))
                 A[0, 0:2] = 1
                 A[1, 1:3] = 1
+                self.check_right = A.shape[1]
+                self.check_left = 0
             if self.orientation == 1 or self.orientation == 3:
                 A = np.zeros((3, 2))
                 A[0:2, 1] = 1
                 A[1:3, 0] = 1
+                self.check_right = 1
+                self.check_left = -1
 
         if self.shape == 5:  # SQUARE
             A = np.ones((2, 2))
+            self.check_right = A.shape[1]
+            self.check_left = -1
 
         return A
 
@@ -117,16 +156,18 @@ class tetris:
     field = np.zeros((sizeX, sizeY))
     points = 0
 
-    def __init__(self, sizeX=10, sizeY=20, points=0):
+    def __init__(self, sizeX=10, sizeY=20, points=0, current_highest=16,current=False):
         self.sizeX = sizeX
         self.sizeY = sizeY
         self.field = np.zeros((sizeX, sizeY))
         self.points = points
+        self.current_highest = current_highest
+        self.current = False
 
     def rotate(self):
         self.block.orientation += 1
         self.block.orientation = self.block.orientation % 4
-
+        A = self.block.get_matrix()
         if self.block.shape == 0 and (self.block.orientation == 0 or self.block.orientation == 2):
             self.block.pos[0] += 2
             self.block.pos[1] -= 2
@@ -142,6 +183,7 @@ class tetris:
                 self.block.pos[1] -= 2
             self.block.orientation -= 1
             self.block.orientation = self.block.orientation % 4
+            A = self.block.get_matrix()
 
         return (self)
 
@@ -154,6 +196,12 @@ class tetris:
             self.block.pos[1] += 1
         return (self)
 
+    def highest(self):
+        for i in range(self.field.shape[0]):
+            if np.sum(self.field[i,:]) < 0:
+                self.current_highest = i
+                break
+
     def translate_right(self):
         self.block.pos[1] += 1
         if self.check_if_not_valid():
@@ -164,10 +212,43 @@ class tetris:
         self.block.pos[0] += 1
         if self.check_if_not_valid():
             A = self.block.get_matrix()
+            i_0 = A.shape[0]
+            j_0 = A.shape[1]
             self.block.pos[0] -= 1
             self.clean()
             self.field[self.block.pos[0]:(self.block.pos[0] + A.shape[0]),
             self.block.pos[1]:(self.block.pos[1] + A.shape[1])] -= A
+
+            if self.block.pos[1] + j_0 < 10 and self.block.pos[1]-1 > -1:
+                    if self.field[self.block.pos[0]+i_0-1, self.block.pos[1]+self.block.check_left] == -1 and self.field[self.block.pos[0]+i_0-1, self.block.pos[1]+self.block.check_right] == -1:
+
+                        self.points += 7
+                        print("middle")
+
+            if self.block.pos[1]+j_0 + 1 == 11 and self.field[self.block.pos[0]+i_0-1, self.block.pos[1]+self.block.check_left] == -1:
+                self.points += 10
+                print("right")
+            if self.block.pos[1]-1 == -1 and self.field[self.block.pos[0]+i_0-1, self.block.pos[1]+self.block.check_right] == -1:
+                self.points += 7
+                print("left")
+
+            if self.block.pos[0] < self.current_highest:
+                penalty = self.current_highest-self.block.pos[0]
+                if self.current == True:
+                    self.points -= 4*penalty
+                    print("double too high", 4 * penalty)
+                else:
+                    self.points -= 2 * penalty
+                    self.current = True
+                    print("too high",2*penalty)
+            if self.block.pos[0] > self.current_highest:
+                self.current = False
+                reward = 2
+                self.points += reward
+                print("low",reward)
+
+            self.highest()
+
             self.new_block()
             self.delete_row()
 
@@ -188,25 +269,27 @@ class tetris:
         return False
 
     def new_block(self):
-        s = np.random.randint(0, 6)
+        #s = np.random.randint(0, 6)
+        s=5
         r = np.random.randint(0, 3)
         self.block = blocks(shape=s, orientation=r, pos=[0, 5])
         if self.check_if_not_valid():
             self.block = None
+            self.points -= 10
             print('YOU LOST, could not generate any more blocks')
             print(self.points)
             return False
         else:
-            self.points += 1
+            self.points += 0
             return True
 
     def delete_row(self):
-        for i in range(self.field.shape[1]):
-            if np.sum(self.field[i, :]) == -10:
+        for i in range(self.field.shape[0]):
+            if np.sum(self.field[i,:]) == -10:
                 print('totally full')
                 self.field = np.delete(self.field, i, axis=0)
                 self.field = np.insert(self.field, 0, 0, axis=0)
-                self.points += 10
+                self.points += 50
 
     def clean(self):
         for i in range(0, self.field.shape[0]):
